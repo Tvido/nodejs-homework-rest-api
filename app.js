f97d830fed287e2eb636a6c70697e029934dd32c
Volodymyr Viter <mad.tvido@gmail.com>
2021-08-27 19:05:41 +0300

    Update app.js

--- app.js
+++ app.js
@@ -14,12 +14,12 @@ app.use(express.json());
 
 app.use("/api/contacts", contactsRouter);
 
-app.use((req, res) => {
+app.use((_, res) => {
   res.status(404).json({ message: "Not found" });
 });
 
-app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message });
+app.use((error, _, res, next) => {
+  res.status(500).json({ message: error.message });
 });
 
 module.exports = app;

15a5c89738b74b2ba06312f6288153d84b1fbe53
Volodymyr Viter <mad.tvido@gmail.com>
2021-08-28 23:28:55 +0300

    Update server.js

--- bin/server.js
+++ bin/server.js
@@ -13,6 +13,7 @@ mongoose
   .then(() => {
     app.listen(PORT, () => {
       console.log(`Server running. Use our API on port: ${PORT}`);
+      console.log("Database connection successful");
     });
   })
   .catch((error) => {

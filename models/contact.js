3d95257c378129a1bda4f4dadfe0c8ebe8ebe736
Volodymyr Viter <mad.tvido@gmail.com>
2021-09-03 23:49:19 +0300

    add models

--- models/contact.js
+++ models/contact.js
@@ -18,6 +18,10 @@ const contactSchema = Schema(
       type: Boolean,
       default: false,
     },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+    },
   },
   { visrionKey: false, timestamps: true }
 );
